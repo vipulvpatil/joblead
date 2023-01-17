97f438cd455aeff66bbd7ff5f552b09506bfd0b2
Vipul Vinod Patil <vipulvpatil@gmail.com>
2023-01-18 01:49:43 +0530

    Tested utf8 file parsing on server

--- pages/api/process_resume.js
+++ pages/api/process_resume.js
@@ -1,7 +1,25 @@
+import formidable from "formidable"
+import fs from "fs"
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+}
+
+
 const ProcessResume = async (req, res) => {
   if(req.method !== "POST"){
     res.status(405).json({error: "method not allowed"})
   } else {
+    const form = new formidable.IncomingForm()
+    form.parse(req, (err, fields, files) => {
+      console.log(files)
+
+      const data = fs.readFileSync(files["resume"].filepath, "utf8")
+      console.log(data)
+    })
+
     res.status(200)
     res.json({result: "okay"})
   }
