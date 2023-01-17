65082b0ae5b1cf0c38dd762fda16080e4c3d79bf
Vipul Vinod Patil <vipulvpatil@gmail.com>
2023-01-18 00:12:52 +0530

    Added a simple api endpoint

---
+++ pages/api/resume.js
@@ -0,0 +1,10 @@
+const ResumeUpload = async (req, res) => {
+  if(req.method !== "POST"){
+    res.status(405).json({error: "method not allowed"})
+  } else {
+    res.status(200)
+    res.json({result: "okay"})
+  }
+}
+
+export default ResumeUpload
