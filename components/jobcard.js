dbcc5eea16dd00cbf61715ed000462cce5f8a760
Vipul Vinod Patil <vipulvpatil@gmail.com>
2023-01-17 20:38:03 +0530

    added text clipping to typograpy

--- components/jobcard.js
+++ components/jobcard.js
@@ -5,9 +5,9 @@ import styles from "@/styles/Home.module.css"
 const Jobcard = ({jobTitle, companyName, jobDescription}) => {
   return (
     <Card variant="outlined" className={styles.card}>
-      <Typography>{jobTitle}</Typography>
-      <Typography>{companyName}</Typography>
-      <Typography>{jobDescription}</Typography>
+      <Typography noWrap={true}>{jobTitle}</Typography>
+      <Typography noWrap={true}>{companyName}</Typography>
+      <Typography className={styles.maxThreeLinesText}>{jobDescription}</Typography>
     </Card>
   )
 }
