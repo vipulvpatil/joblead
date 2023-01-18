5d32d36a855b12def712975229d27faa32155f26
Vipul Vinod Patil <vipulvpatil@gmail.com>
2023-01-18 11:36:00 +0530

    Refactor: Added a lib for File Parsing

--- pages/api/process_resume.js
+++ pages/api/process_resume.js
@@ -1,5 +1,5 @@
+import FileParser from "@/lib/file_parser"
 import formidable from "formidable"
-import mammoth from "mammoth"
 
 export const config = {
   api: {
@@ -13,11 +13,9 @@ const ProcessResume = async (req, res) => {
   } else {
     const form = new formidable.IncomingForm()
     form.parse(req, async (err, fields, files) => {
-      const filepath = files["resume"].filepath
-      console.log(files["resume"].originalFilename)
-      
-      const data = await mammoth.extractRawText({path: filepath})
-      console.log(data)
+      if (files && files["resume"]){
+        FileParser.identifyFileType(files["resume"])
+      }
     })
 
     res.status(200)
