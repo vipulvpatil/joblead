4e97833d7ddf7d9868d7569a7f158ab41aa2beaa
Vipul Vinod Patil <vipulvpatil@gmail.com>
2023-01-21 12:59:29 +0530

    Restructured API call to be in the tabContainer

--- components/tab_container.js
+++ components/tab_container.js
@@ -7,15 +7,42 @@ import styles from "@/styles/Home.module.css"
 const TabContainer = () => {
   const [tabContent, setTabContent] = useState(<UploadDiv/>)
   const [value, setValue] = useState(0)
+  const [apiError, setApiError] = useState("")
 
-  const onChange = (e, newValue) => {
+  const onTabChange = (e, newValue) => {
     setValue(newValue)
   }
 
+  const uploadResume = async(resumeFile) => {
+    const formData = new FormData()
+    formData.append("resume", resumeFile)
+
+    const config = {
+      headers: {"content-type": "multipart/form-data"},
+      onUploadProgress: (event) => {
+        console.log("Current progress:", Math.round((event.loaded * 100) / event.total))
+      },
+    }
+
+    const response = await fetch("/api/analyse_resume", {
+      method: "POST",
+      body: formData,
+    }, config)
+    const responseJson = await response.json()
+    setApiError(responseJson["error"])
+  }
+
   useEffect(() => {
+    const onFileSelectChange = (event) => {
+      if (!event.target.files?.length) {
+        return
+      }
+      uploadResume(event.target.files[0])
+    }
+
     switch(value) {
       case 0:
-        setTabContent(<UploadDiv/>)
+        setTabContent(<UploadDiv onChange={onFileSelectChange} apiError={apiError}/>)
         break
       case 1:
         setTabContent(<PersonaDiv/>)
@@ -24,10 +51,10 @@ const TabContainer = () => {
         setTabContent(<div>3</div>)
         break
     }
-  }, [value])
+  }, [value, apiError])
 
   return <>
-    <Tabs value={value} onChange={onChange} centered className={styles.subheader}>
+    <Tabs value={value} onChange={onTabChange} centered className={styles.subheader}>
       <Tab label={<Typography variant="tab">Upload</Typography>} />
       <Tab label={<Typography variant="tab">Persona</Typography>} />
       <Tab label={<Typography variant="tab">About</Typography>} />
--- components/upload_div.js
+++ components/upload_div.js
@@ -1,37 +1,8 @@
 import {Button, Stack, Typography} from "@mui/material"
 import UploadIcon from "@mui/icons-material/Upload"
 import styles from "@/styles/Home.module.css"
-import {useState} from "react"
 
-const UploadDiv = () => {
-  const [apiError, setApiError] = useState("")
-
-  const onChange = (event) => {
-    if (!event.target.files?.length) {
-      return
-    }
-    uploadResume(event.target.files[0])
-  }
-
-  const uploadResume = async(resumeFile) => {
-    const formData = new FormData()
-    formData.append("resume", resumeFile)
-
-    const config = {
-      headers: {"content-type": "multipart/form-data"},
-      onUploadProgress: (event) => {
-        console.log("Current progress:", Math.round((event.loaded * 100) / event.total))
-      },
-    }
-
-    const response = await fetch("/api/analyse_resume", {
-      method: "POST",
-      body: formData,
-    }, config)
-    const responseJson = await response.json()
-    setApiError(responseJson["error"])
-  }
-
+const UploadDiv = ({onChange, apiError}) => {
   return (
     <Stack direction="column" className={styles.uploadFileStack} spacing={2}>
       <Typography variant="body2">Begin by uploading your resume.</Typography>
