2e43d0ce0702ff86e1372ca0b84fd14122012249
Vipul Vinod Patil <vipulvpatil@gmail.com>
2023-01-22 13:57:45 +0530

    Formatted date to local timezone

--- components/jobcard.js
+++ components/jobcard.js
@@ -3,6 +3,7 @@ import styles from "@/styles/Home.module.css"
 
 
 const Jobcard = ({title, company, date, locations, salary, url}) => {
+  var localDate = new Date(date)
   return (
     <a target="_blank" href={url} rel="noopener noreferrer">
       <Card variant="outlined" className={styles.card}>
@@ -10,7 +11,7 @@ const Jobcard = ({title, company, date, locations, salary, url}) => {
         <Typography variant="h5" noWrap={true}>{company}</Typography>
         <Typography variant="body" className={`${styles.limitedLinesText} ${styles.jobDescription}`}>{salary}</Typography>
         <Typography variant="body" className={`${styles.limitedLinesText} ${styles.jobDescription}`}>{locations}</Typography>
-        <Typography variant="body" className={`${styles.limitedLinesText} ${styles.jobDescription}`}>{date}</Typography>
+        <Typography variant="body" className={`${styles.limitedLinesText} ${styles.jobDescription}`}>{localDate.toLocaleString()}</Typography>
       </Card>
     </a>
   )
