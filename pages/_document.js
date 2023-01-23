a0e1b1d4d2f2a7a6ef59efcf03de6528e476da4a
Vipul Vinod Patil <vipulvpatil@gmail.com>
2023-01-23 17:57:52 +0530

    Added google ads

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,9 @@ import {Head, Html, Main, NextScript} from "next/document"
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7093895028114761" crossOrigin="anonymous"></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
