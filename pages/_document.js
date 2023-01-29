580afb0824b46b40dbe94d4d6553fec3a535afa0
Vipul Vinod Patil <vipulvpatil@gmail.com>
2023-01-29 13:47:11 +0530

    Replaced Google analytics tag with Google Tag Manager tag instead

--- pages/_document.js
+++ pages/_document.js
@@ -6,25 +6,25 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7093895028114761" crossorigin="anonymous"></script>
-        <HTMLComment text="Global site tag (gtag.js) - Google Analytics"/>
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-JQM7067RRX"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
+        <HTMLComment text="Google Tag Manager"/>
+        <Script id="google-tag-manager-head" strategy="afterInteractive">
           {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){window.dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'G-JQM7067RRX');
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-P7NPZMC');
           `}
         </Script>
+        <HTMLComment text="End Google Tag Manager"/>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7093895028114761" crossOrigin="anonymous"></script>
+
       </Head>
       <div>
       </div>
       <body>
+        <HTMLComment text="Google Tag Manager (noscript)"/>
+          <noscript>
+            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P7NPZMC" height="0" width="0" style={{display:"none", visibility:"hidden"}}>
+            </iframe>
+          </noscript>
+        <HTMLComment text="End Google Tag Manager (noscript)"/>
         <Main />
         <NextScript />
       </body>
